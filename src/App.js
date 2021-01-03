9a52231bd7316b2a0365e1706fe7435b4907646d
WangZhongxi <wangzhongxi@Daniel-ZXWdeMacBook-Pro.local>
2021-01-03 14:50:10 +1100

    added router axios

--- src/App.js
+++ src/App.js
@@ -1,31 +1,35 @@
 import logo from './logo.svg';
+import React from 'react';
 import './App.scss';
 import { userLogin } from './actions/user';
 import { connect } from 'react-redux';
 
-function App({ user, userLogin }) {
-  console.log(user)
-  console.log(typeof userLogin)
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+@connect(({ user }) => ({ user }), { userLogin })
+class App extends React.Component {
+  render() {
+    const {user, userLogin} = this.props;
+    console.log(user);
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          haha
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            haha
         </a>
-        <button onClick={() => userLogin(12)}>click</button>
-      </header>
-    </div>
-  );
+          <button onClick={() => userLogin(12)}>click</button>
+        </header>
+      </div>
+    )
+  };
 }
 
-const actualApp = connect(({ user }) => ({ user }), { userLogin })(App);
-export default actualApp;
+
+export default App;
