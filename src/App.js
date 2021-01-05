916e15b8fa7eaa8b9aebc91b75a4421ca2d9407a
WangZhongxi <wangzhongxi@Daniel-ZXWdeMacBook-Pro.local>
2021-01-05 20:55:23 +1100

    added point animation

--- src/App.js
+++ src/App.js
@@ -4,18 +4,23 @@ import { Switch, Route, Redirect } from 'react-router-dom';
 import Home from './Pages/Home'
 import PrivateRoute from './Components/PrivateRoute'
 import NotFound from '@/Pages/NotFound'
+import { invokePoints } from '@/utils/pointsAnimation'
 
 class App extends React.Component {
+  componentDidMount() {
+    invokePoints();
+  }
+
   render() {
     return (
       <Switch>
         <Route path='/home' component={Home}></Route>
-        <Route path='/login' component={()=>(<div>please login</div>)}></Route>
+        <Route path='/login' component={() => (<div>please login</div>)}></Route>
         <Route path='/detail' exact component={null}></Route>
         <Route path='/detail/:id' component={null}></Route>
-        <Redirect to="/home" from='/' exact/>
-        <PrivateRoute path='/user' component={()=>(<div>rekt</div>)}/>
-        <Route component={NotFound}/>
+        <Redirect to="/home" from='/' exact />
+        <PrivateRoute path='/user' component={() => (<div>rekt</div>)} />
+        <Route component={NotFound} />
       </Switch>)
   };
 }
