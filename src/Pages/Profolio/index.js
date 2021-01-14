6164964b08d9799820e50552b2aa8ef0239fe233
WangZhongxi <wangzhongxi@Daniel-ZXWdeMacBook-Pro.local>
2021-01-14 20:47:01 +1100

    profolio page layout complete

--- src/Pages/Profolio/index.js
+++ src/Pages/Profolio/index.js
@@ -1,6 +1,24 @@
 import { Component } from 'react';
 import { invokePoints } from '@/utils/pointsAnimation'
 import './index.scss';
+
+const profolioProjects = [
+    {
+        Name: 'JR Dashboard',
+        Description: 'An online learning management portal, used by students, tutors, professional training instructors, marketing team, and academy management',
+        Role: 'Full Stack developer',
+        Pic: '',
+        Link: '',
+        'Tech-stack': {
+            'Front-end': 'HTML5, CSS3, Less, JavaScript(ES6 +), React, Redux, Redux-saga, UmiJs, DvaJs, Ant Design, Zeplin',
+            'Back-end': 'Node.js, KeystoneJs(express), Restful API, JWT, MongoDB',
+            'cloud': 'Elastic Cache, Alicloud-video, serverless, aws-s3',
+            'tooling': 'Git, NPM, Bitbucket, JIRA, Postman, Swagger-doc, webpack',
+            'Methodologies': 'Agile/Scrum CI/CD',
+        }
+    }
+];
+
 export default class Profolio extends Component {
     componentDidMount() {
         invokePoints();
@@ -8,6 +26,19 @@ export default class Profolio extends Component {
     render() {
         return (
             <div className='portfolio'>
+                <ul className='project-showcase__container'>
+                    {profolioProjects.map((project) => {
+                        return (<li className='project-showcase__wrapper border-professioanal'>
+                            <div className='project-showcase--pic'>hahahhahah</div>
+                            <div className='project-showcase__article'>
+                                <div className='project-showcase__description'>{project.Description}
+                                </div>
+                                <div className='project-showcase__role'>{project.Role}</div>
+                                <div className='project-showcase__techstack'>{project['Tech-stack']['Front-end']}</div>
+                            </div>
+                        </li>)
+                    })}
+                </ul>
             </div>
         )
     }
